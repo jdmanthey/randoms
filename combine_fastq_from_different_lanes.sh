1ee1ed406289804292db1137b8c2b2ac85ecd299
Joe Manthey <jdmanthey@gmail.com>
2023-03-21 10:44:55 -0500

    Update combine_fastq_from_different_lanes.sh

--- combine_fastq_from_different_lanes.sh
+++ combine_fastq_from_different_lanes.sh
@@ -1,6 +1,6 @@
 # take base names of all files to combine all 4 lanes of sequencing into one file each read
 while read -r basename; do
-	cat ${basename}*R1_* >> ../${basename}_R1.fastq.gz
-	cat ${basename}*R2_* >> ../${basename}_R2.fastq.gz
+	cat ${basename}_*R1_* >> ../${basename}_R1.fastq.gz
+	cat ${basename}_*R2_* >> ../${basename}_R2.fastq.gz
 done < basenames.txt
 
