fe064ee7568b70bde1111a0b5c3fdb8779102980
Joe Manthey <jdmanthey@gmail.com>
2023-06-30 16:01:35 -0500

    Update combine_fastq_from_different_lanes.sh

--- combine_fastq_from_different_lanes.sh
+++ combine_fastq_from_different_lanes.sh
@@ -4,3 +4,12 @@ while read -r basename; do
 	cat ${basename}_*R2_* >> ../${basename}_R2.fastq.gz
 done < basenames.txt
 
+# alternative method with numbered individuals
+for i in {1..52}; do 
+	for j in $( ls ${i}_*R1* ); do
+ 	cat $j >> /path/to/output/${i}_R1.fastq.gz; done
+  	for k in $( ls ${i}_*R2* ); do
+ 	cat $k >> /path/to/output/${i}_R2.fastq.gz; done
+done
+
+
